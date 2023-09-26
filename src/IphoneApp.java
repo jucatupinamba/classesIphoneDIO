a008b8ed1f5b1c1fed43b272e31a39d06cd44db8
Juarez Martins <juniorbmx13@gmail.com>
2023-09-26 20:22:17 -0300

    feat: App modelo finalizado.

--- src/IphoneApp.java
+++ src/IphoneApp.java
@@ -13,78 +13,95 @@ public class IphoneApp {
 
         Scanner scan = new Scanner(System.in);
         int numero;
+        int opcao;
 
         System.out.println("==== Iphone Juca - Simples como Java =====");
         System.out.println("Digite os seguintes números para ativar os aplicativos: ");
         System.out.println("1 - Aparelho Telefônico, 2 - Navegador da Internet, 3 - Reprodutor Musical");
-        int opcao = scan.nextInt();
+        opcao = scan.nextInt();
 
-        while(opcao != 0){
-            if(opcao == 1){
+        while (opcao != 0) {
+            while (opcao == 1) {
                 System.out.println("Para usar o aparelho telefônico utilize os seguintes números");
-                System.out.println("Digite 1 - Ligar, 2 - Atender, 3 - Desligar, 0 - Sair desse App");
+                System.out.println("Digite 1 - Ligar, 2 - Atender, 3 - Desligar, " +
+                        "9 - Menu Anterior, 0 - Sair desse App");
                 numero = scan.nextInt();
-                switch (numero){
-                    case 1:
-                        aparelhoTelefonico.ligar();
-                        break;
-
-                    case 2:
-                        aparelhoTelefonico.atender();
-                        break;
-
-                    case 3:
-                        aparelhoTelefonico.desligar();
-
-                    case 0:
-                        opcao = 0;
-                        break;
+                if(numero == 1){
+                    System.out.println(aparelhoTelefonico.ligar());
+                    System.out.println();
                 }
-        }
+                if(numero == 2){
+                    System.out.println(aparelhoTelefonico.atender());
+                    System.out.println();
+                }
+                if(numero == 3){
+                    System.out.println(aparelhoTelefonico.desligar());
+                    System.out.println();
+                }
+                if(numero == 9){
+                    opcao = 9;
+                    break;
+                }
+                if(numero == 0){
+                    opcao = 0;
+                    break;
+                }
+            }
 
-            if(opcao == 2){
+            while (opcao == 2) {
                 System.out.println("Para usar o navegador da internet utilize os seguintes números");
                 System.out.println("Digite 1 - Exibir Pagina, 2 - Atualizar Pagina, " +
-                        "3 - Add Nova Aba, 0 - Sair desse App");
+                        "3 - Add Nova Aba, 9 - Menu Anterior, 0 - Sair desse App");
                 numero = scan.nextInt();
-                switch (numero){
-                    case 1:
-                        navegadorDaInternet.exibirPagina();
-                        break;
 
-                    case 2:
-                        navegadorDaInternet.atualizarPagina();
-                        break;
-
-                    case 3:
-                        navegadorDaInternet.addNovaAba();
-                        break;
-
-                    case 0:
-                        opcao = 0;
-                        break;
+                if (numero == 1) {
+                    System.out.println(navegadorDaInternet.exibirPagina());
+                    System.out.println();
+                }
+                if (numero == 2) {
+                    System.out.println(navegadorDaInternet.atualizarPagina());
+                    System.out.println();
+                }
+                if (numero == 3) {
+                    System.out.println(navegadorDaInternet.addNovaAba());
+                    System.out.println();
+                }
+                if(numero == 9){
+                    opcao = 9;
+                    break;
+                }
+                if (numero == 0) {
+                    opcao = 0;
+                    break;
                 }
             }
-
-
-            if(opcao == 3){
+            while (opcao == 3) {
                 System.out.println("Para usar o reprodutor musical utilize os seguintes números");
-                System.out.println("Digite 1 - Tocar Muscia, 2 - Pausar Musica, 0 - Sair desse App");
+                System.out.println("Digite 1 - Tocar Muscia, 2 - Pausar Musica, " +
+                        "9 - Menu Anterior, 0 - Desligar o Celular");
                 numero = scan.nextInt();
-                switch (numero){
-                    case 1:
-                        reprodutorMusical.tocarMusica();
-                        break;
-
-                    case 2:
-                        reprodutorMusical.pausarMusica();
-                        break;
-
-                    case 0:
-                        opcao = 0;
-                        break;
+                if (numero == 1) {
+                    System.out.println(reprodutorMusical.tocarMusica());
+                    System.out.println();
                 }
+                if (numero == 2) {
+                    System.out.println(reprodutorMusical.pausarMusica());
+                    System.out.println();
+                }
+                if(numero == 9){
+                    opcao = 9;
+                    break;
+                }
+                if (numero == 0) {
+                    opcao = 0;
+                    break;
+                }
+            }
+            while(opcao == 9){
+                System.out.println("1 - Aparelho Telefônico, 2 - Navegador da Internet, 3 - Reprodutor Musical");
+                opcao = scan.nextInt();
             }
         }
+        System.out.println("IPHONE JUCA - DESLIGANDO");
     }
 }
